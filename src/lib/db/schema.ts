6d3374396e8f9813820c76891a472db1d455a6d8
jun-hash <wnstjsy@korea.ac.kr>
2023-12-10 02:54:15 -0500

    :sparkles: feat(be): tasks schema 생성
    <description>
    
    <issue-number>

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -10,14 +10,32 @@ export const $notes = pgTable("notes", {
     editorState: text("editor_state"),
   });
 
-  export type NoteType = {
-    id: number,
-    name: string,
-    createdAt: Date,
-    imageurl: string,
-    userId: string,
-    editorState: string,
-  }
+export type NoteType = {
+  id: number,
+  name: string,
+  createdAt: Date,
+  imageurl: string,
+  userId: string,
+  editorState: string,
+}
+
+export const $tasks = pgTable("tasks", {
+    id: serial("id").primaryKey(),
+    title: text("title").notNull(),
+    status:text("status", { enum: ["Backlog", "Todo", "In Progress", "Done", "Canceled"] }),
+    label: text("label"),
+    priority: text("priority", { enum: ["Low", "Medium", "High"] }),
+    userId: text("user_id").notNull(),
+})
+
+export type TaskType = {
+  id: number,
+  title: string,
+  status: "Backlog"| "Todo"| "In Progress"| "Done"| "Canceled",
+  label: string,
+  priority: "Low"| "Medium"| "High",
+  userId: string,
+}
 
 // drizzle-orm
 // drizzle-kit
