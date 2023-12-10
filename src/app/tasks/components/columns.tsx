baad324c0372299fd53003f3208440686cf835a2
jun-hash <wnstjsy@korea.ac.kr>
2023-12-10 02:55:13 -0500

    :sparkles: feat(fe): tasks data table 구현 #5
    <description>
    
    <issue-number>

---
+++ src/app/tasks/components/columns.tsx
@@ -0,0 +1,94 @@
+"use client"
+import { ColumnDef } from "@tanstack/react-table"
+import { TaskType } from "@/lib/db/schema"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Checkbox } from "@/components/ui/checkbox"
+
+
+export const columns: ColumnDef<TaskType>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+          <Checkbox
+            checked={
+              table.getIsAllPageRowsSelected() ||
+              (table.getIsSomePageRowsSelected() && "indeterminate")
+            }
+            onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+            aria-label="Select all"
+          />
+        ),
+        cell: ({ row }) => (
+          <Checkbox
+            checked={row.getIsSelected()}
+            onCheckedChange={(value) => row.toggleSelected(!!value)}
+            aria-label="Select row"
+          />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+      accessorKey: "title",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Title
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+    },
+    {
+      accessorKey: "status",
+      header: "Status",
+    },
+    {
+      accessorKey: "label",
+      header: "Label",
+    },
+    {
+        accessorKey: "priority",
+        header: "Priority",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+          const task = row.original
+     
+          return (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                  <span className="sr-only">Open menu</span>
+                  <MoreHorizontal className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                <DropdownMenuItem
+                  onClick={() => navigator.clipboard.writeText(task.title)}
+                >
+                  Copy Title
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>View test</DropdownMenuItem>
+                <DropdownMenuItem>View test two</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )
+        },
+    },
+  ]
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import { db } from "@/lib/db";
-import { $notes } from "@/lib/db/schema";
+import { $tasks, TaskType } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
 import { UserButton, auth  } from '@clerk/nextjs'
+import { columns } from "./components/columns"
+import { DataTable } from "./components/DataTable"
 
-type Props = {}
 
-const TasksPage = (props: Props) => {
+
+const TasksPage  =  async () => {
     const { userId } = auth()
+    const tasks :TaskType[] = await db
+    .select()
+    .from($tasks)
+    .where(eq($tasks.userId, userId!));
+
+    
     return (
-    <div>page {userId}</div>
+        <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+            <div className="flex items-center justify-between space-y-2">
+                <div>
+                    <h2 className="text-2xl font-bold tracking-tight">Tasks!</h2>
+                    <p className="text-muted-foreground">
+                    Here&apos;s a list of your tasks for this month!
+                    </p>
+                </div>
+                <div className="flex items-center space-x-2">
+                    <UserButton />
+                </div>
+            </div>
+            <DataTable data={tasks} columns={columns} />
+        </div>
     )
 }
 
